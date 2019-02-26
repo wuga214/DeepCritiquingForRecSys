4e25c063219b96d05f573243c60ff91d1e8061ad
wuga <wuga214@gmail.com>
2019-02-26 18:01:56 -0500

    performance baseline

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from utils.progress import WorkSplitter, inhour
 from utils.argcheck import check_float_positive, check_int_positive, shape
 
 from models.interpautorec import InterpretableAutoRec
+from models.autorec import AutoRec
 from predicts.itembased import topk_predict
 from metrics.general_performance import evaluate
 
@@ -21,18 +22,24 @@ def main(args):
 
     n, m, k = Rtrain.shape # Item, User, Text Feature
 
-    progress.section("Train Model")
-    iae = InterpretableAutoRec([m, k], args.rank, 100, args.lamb) # I-AutoRec
-    iae.train_model(Rtrain, args.epoch)
+    # progress.section("Train Model")
+    # iae = InterpretableAutoRec([m, k], args.rank, 100, args.lamb) # I-AutoRec
+    # iae.train_model(Rtrain, args.epoch)
+    #
+    # progress.section("Prediction")
+    # predicted = topk_predict(iae, Rvalid, args.topk, n, m, k)
+    #
+    # metric_names = ['R-Precision', 'NDCG', 'Clicks', 'Recall', 'Precision']
+    # result = evaluate(predicted[:, :, 0], Rtrain[:, :, 0].tocsr().transpose(), metric_names, [args.topk])
+    # print("-")
+    # for metric in result.keys():
+    #     print("{0}:{1}".format(metric, result[metric]))
 
-    progress.section("Prediction")
-    predicted = topk_predict(iae, Rvalid, args.topk, n, m, k)
+    model = AutoRec(n, args.rank, 100, lamb=args.lamb)
+    model.train_model(Rtrain[:, :, 0].transpose(), args.epoch)
 
-    metric_names = ['R-Precision', 'NDCG', 'Clicks', 'Recall', 'Precision']
-    result = evaluate(predicted[:, :, 0], Rtrain[:, :, 0].tocsr().transpose(), metric_names, [args.topk])
-    print("-")
-    for metric in result.keys():
-        print("{0}:{1}".format(metric, result[metric]))
+
+
 
 
 if __name__ == "__main__":
